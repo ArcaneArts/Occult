6f0b77fdd5d87b13a9a8736c3090345b18324114
cyberpwn <dan@arcane.art>
2025-02-22 17:40:54 -0600

    Fix indexes

--- occult/lib/task/gen_firestore_indexes.dart
+++ occult/lib/task/gen_firestore_indexes.dart
@@ -4,6 +4,6 @@ class TGenFirestoreIndexes extends OTaskJob {
   TGenFirestoreIndexes() : super("Generate firestore.indexes.json");
 
   @override
-  Future<void> run() => add(TMakeJson("config/firebase/firebase.indexes.json",
+  Future<void> run() => add(TMakeJson("config/firebase/firestore.indexes.json",
       {"indexes": [], "fieldOverrides": []}));
 }
